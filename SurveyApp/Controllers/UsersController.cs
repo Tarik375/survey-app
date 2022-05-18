1e0c498c77f1fb26e9f0bdc59a71b458e936b44f
Alma Kasumović <akasumovic1@etf.unsa.ba>
2022-05-18 13:52:41 +0000

    Merged in feature/registration (pull request #2)
    registracija
    
    * registracija
    
    * add getters and setters
    
    
    Approved-by: Enes Kapetanović

--- SurveyApp/Controllers/UsersController.cs
+++ SurveyApp/Controllers/UsersController.cs
@@ -9,5 +9,10 @@ namespace SurveyApp.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult SubmitRegistration()
+        {
+            return Redirect("/Auth/Login");
+        }
     }
 }
---
+++ SurveyApp/Models/Users/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace SurveyApp.Models.Users
+{
+    public class RegisterViewModel
+    {
+
+
+        public string? Username
+        { 
+            get; set;
+        }
+
+        public string? Email
+        {
+            get; set;
+        }
+
+        public string? Password
+        {
+            get; set;
+        }
+         public string? PasswordConfirm
+        {
+            get; set;
+           
+        }
+
+
+
+    }
+}
