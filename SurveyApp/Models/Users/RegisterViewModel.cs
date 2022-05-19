b3fc404eefc853b6bd7716081acdf472b7fd8e39
Alma <akasumovic1@etf.unsa.ba>
2022-05-19 11:19:34 +0200

    Izmjena Register.cshtml view-a

--- SurveyApp/Models/Users/RegisterViewModel.cs
+++ SurveyApp/Models/Users/RegisterViewModel.cs
@@ -2,27 +2,13 @@
 {
     public class RegisterViewModel
     {
+        public string? Username{ get; set; }
 
+        public string? Email{get; set;  }
 
-        public string? Username
-        { 
-            get; set;
-        }
+        public string? Password{ get; set;   }
 
-        public string? Email
-        {
-            get; set;
-        }
-
-        public string? Password
-        {
-            get; set;
-        }
-         public string? PasswordConfirm
-        {
-            get; set;
-           
-        }
+        public string? PasswordConfirm{  get; set;  }
 
 
 
