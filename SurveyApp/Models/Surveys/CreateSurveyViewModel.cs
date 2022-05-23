71acf7d85b22a6ebe34d0e3a4b42e040c9f69c48
Tarik Mekić <tmekic1@etf.unsa.ba>
2022-05-23 14:15:20 +0200

    popravka

--- SurveyApp/Models/Surveys/CreateSurveyViewModel.cs
+++ SurveyApp/Models/Surveys/CreateSurveyViewModel.cs
@@ -6,9 +6,9 @@ namespace SurveyApp.Models.Surveys
         [Required(ErrorMessage = "Name is required.")]
         [Range(3, 20, ErrorMessage = "Name must be between 3 and 20 characters long.")]
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [MaxLength(50, ErrorMessage = "Description can't be longer than 50 characters.")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
     }
 }
