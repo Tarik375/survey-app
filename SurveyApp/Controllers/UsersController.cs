a023a18f634760a34107256afa43684e238126c6
Alma <akasumovic1@etf.unsa.ba>
2022-05-25 14:22:23 +0200

    fix codestyle

--- SurveyApp/Controllers/UsersController.cs
+++ SurveyApp/Controllers/UsersController.cs
@@ -27,10 +27,9 @@ namespace SurveyApp.Controllers
                 return View("Register", model);
             }
 
-            
-                List<User> users = _db.Users
-                .Where(x => x.Username.ToLower() == model.Username.ToLower() || x.Email.ToLower() == model.Email.ToLower())
-                .ToList();
+            List<User> users = _db.Users
+                                        .Where(x => x.Username.ToLower() == model.Username.ToLower() || x.Email.ToLower() == model.Email.ToLower())
+                                        .ToList();
 
             if (users.Any(x => x.Email.ToLower() == model.Email.ToLower()))
             {
@@ -47,15 +46,11 @@ namespace SurveyApp.Controllers
                 return View("Register", model);
             }
 
-
             var user = new User()
             {
-
                 Email = model.Email,
                 Username = model.Username,
                 PasswordHash = BCrypt.Net.BCrypt.HashPassword(model.Password)
-
-
             };
 
             _db.Users.Add(user);
@@ -63,83 +58,10 @@ namespace SurveyApp.Controllers
 
             return Redirect("/Auth/Login");
         }
-       
+
 
     }
 }
 
 
 
-
-/*var model1 = await _db.Users.FirstOrDefaultAsync(x => x.Email.ToLower() == model.Email.ToLower()
-              || x.Username.ToLower() == model.Username.ToLower());
-            
-
-            if (!(model1 == null) ||  !BCrypt.Net.BCrypt.Verify(model.Email, model1.Email))
-            {
-              //if(_db.Users.Where(x => x.Email == model.Email))
-                ModelState.AddModelError(string.Empty, "This email is already taken.");
-              
-            }
-            if (!(model1 == null) || !BCrypt.Net.BCrypt.Verify(model.Username, model1.Username))
-            {
-                //if(_db.Users.Select(x => x.Email == model.Email))
-                ModelState.AddModelError(string.Empty, "This username is already taken.");
-
-            }
-            if (!(model1 == null) || ( !BCrypt.Net.BCrypt.Verify(model.Email, model1.Email) && !BCrypt.Net.BCrypt.Verify(model.Username, model1.Username)))
-            {
-                //if(_db.Users.Where(x => x.Email == model.Email && x.Username == model.Username))
-                ModelState.AddModelError(string.Empty, "This email and username is already taken.");
-                 
-            }
-                        
-            return View(model1);*/
-
-/* var user = new User()
- {
-
-     Email = model.Email,   
-     Username = model.Username,
-     PasswordHash = BCrypt.Net.BCrypt.HashPassword(model.Password)
-
-
- };
-
- _db.Users.Add(user);
- _db.SaveChanges();*/
-// return Redirect("/Auth/Login");
-
-/*else if(ModelState.IsValid)
-            {
-                if (!UsersController.Exists(RegisterViewModel.Username))
-                {
-                    if (!user.EmailExists(RegisterViewModel.Email))
-                    {
-                        _db.Add(
-                            new User
-                            {
-                                Created = DateTime.Now,
-                                Email = RegisterViewModel.Email,
-                                Password = RegisterViewModel.Password,
-                                Username = RegisterViewModel.Username
-                            });
-
-                        _db.SaveChanges();
-                        TempData["RegistrationDetails"] = RegisterViewModel;
-
-                        return RedirectToAction("Confirm");
-                    }
-                    else
-                    {
-                        ModelState.AddModelError(string.Empty, "This email is already taken.");
-                    }
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "This username is already taken.");
-                }
-            }
-
-            return View(RegisterViewModel);
-        }*/
