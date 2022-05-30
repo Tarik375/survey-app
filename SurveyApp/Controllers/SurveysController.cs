cddc84f51b3acd3a0bb9a6954d80aab366760357
Tarik Mekić <tmekic1@etf.unsa.ba>
2022-05-30 11:48:46 +0200

    popravka 2.0

--- SurveyApp/Controllers/SurveysController.cs
+++ SurveyApp/Controllers/SurveysController.cs
@@ -14,7 +14,7 @@ namespace SurveyApp.Controllers
         {
             _db = db;
         }
-        
+
         [HttpGet]
         public IActionResult Create()
         {
@@ -22,43 +22,38 @@ namespace SurveyApp.Controllers
         }
         [HttpPost]
         public IActionResult SubmitSurvey([FromForm] CreateSurveyViewModel model)
-        {   
-            if(!ModelState.IsValid)
+        {
+            if (!ModelState.IsValid)
             {
                 return View("Create", model);
             }
-            
-            
-                long userId = long.Parse(HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
-                var survey = new Survey()
-                {
-                    Name = model.Name,
-                    Description = model.Description,
-                    CreatedDate = DateTime.UtcNow,
-                    UserId = userId,
-                };
-                _db.Surveys.Add(survey);
-                _db.SaveChanges();
-            
+            long userId = long.Parse(HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            var survey = new Survey()
+            {
+                Name = model.Name,
+                Description = model.Description,
+                CreatedDate = DateTime.UtcNow,
+                UserId = userId,
+            };
+            _db.Surveys.Add(survey);
+            _db.SaveChanges();
+
             return Redirect("/Surveys/Index");
         }
-
         [HttpGet]
         public IActionResult Index()
         {
-            long userId = long.Parse(HttpContext.User.Claims
-                .FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier).Value);
-            
-            List<Survey> surveys =  _db.Surveys.Where(survey => survey.Id == userId).ToList();
+            long userId = long.Parse(HttpContext.User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier).Value);
+            List<Survey> surveys = _db.Surveys.Where(survey => survey.Id == userId).ToList();
             SurveyIndexViewModel surveyIndex = new SurveyIndexViewModel();
-            surveyIndex.Surverys = new List<SurveyItemViewModel>(); 
+            surveyIndex.Surverys = new List<SurveyItemViewModel>();
             surveys.ForEach(survey =>
             {
                 SurveyItemViewModel surveyItem = new SurveyItemViewModel();
                 surveyItem.Id = survey.Id;
                 surveyItem.Name = survey.Name;
                 surveyItem.Description = survey.Description;
-                surveyItem.CreatedDate = survey.CreatedDate; 
+                surveyItem.CreatedDate = survey.CreatedDate;
                 surveyIndex.Surverys.Add(surveyItem);
             });
             return View(surveyIndex);
