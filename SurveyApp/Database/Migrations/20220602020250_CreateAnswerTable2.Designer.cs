e0645ae2157c88e131a3644d4edb92659a32b179
Emir <eskalonjic2@etf.unsa.ba>
2022-06-02 04:15:22 +0200

    promena3

--- SurveyApp/Database/Migrations/20220602020250_CreateAnswerTable2.Designer.cs
+++ SurveyApp/Database/Migrations/20220602020250_CreateAnswerTable2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SurveyApp.Database;
 
@@ -11,9 +12,10 @@ using SurveyApp.Database;
 namespace SurveyApp.Database.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220602020250_CreateAnswerTable2")]
+    partial class CreateAnswerTable2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SurveyApp/Database/Migrations/20220602020250_CreateAnswerTable2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyApp.Database.Migrations
+{
+    public partial class CreateAnswerTable2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_QuestionId",
+                table: "Answers",
+                column: "QuestionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Answers_Questions_QuestionId",
+                table: "Answers",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Answers_Questions_QuestionId",
+                table: "Answers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Answers_QuestionId",
+                table: "Answers");
+        }
+    }
+}
