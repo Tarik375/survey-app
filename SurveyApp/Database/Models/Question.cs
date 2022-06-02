4258181d137d6e77fd3fcf6ae4d7be0b2716d2aa
Enes Kapetanovic <enes.kapetanovic@klika.ba>
2022-06-02 08:25:06 +0200

    fix question table

--- SurveyApp/Database/Models/Question.cs
+++ SurveyApp/Database/Models/Question.cs
@@ -10,6 +10,6 @@
         public long SurveyId { get; set; }
 
         public Survey? Survey { get; set; }
-        public List<Question> Questions { get; set; }
+        public List<Answer> Answers { get; set; }
     }
 }
